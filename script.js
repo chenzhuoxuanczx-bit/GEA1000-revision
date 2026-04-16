f558799bd9ff7e0d2933763fac98808716e65242
zhuoxuan <chenzhuoxuan.czx@gmail.com>
2026-04-16 22:07:00 +0800

    feat: project scaffold with dark theme shell

---
+++ script.js
@@ -0,0 +1,8 @@
+// GEA1000 Study Companion
+const app = document.getElementById('app');
+
+function render(html) {
+  app.innerHTML = html;
+}
+
+render('<h1>GEA1000 Study Companion</h1><p>Loading...</p>');
